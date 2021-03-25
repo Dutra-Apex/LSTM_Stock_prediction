4048be995dd937c360036048ece9230f75aaddcf
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-03-24 21:20:03 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -3,3 +3,4 @@
 model = Sequential()
 # Model with 50 neurons, return_sequences = True because we are using another LSTM layer
 # Input shape takes the number of time steps(train.shape[1]) and number of features (1)
+# Number of features is simply the closing price
