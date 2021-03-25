0f7af02e7db483dc40d23f54ac0ead21e3e85a18
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-03-24 21:19:46 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,3 +1,5 @@
 # Build the LSTM Model
 
 model = Sequential()
+# Model with 50 neurons, return_sequences = True because we are using another LSTM layer
+# Input shape takes the number of time steps(train.shape[1]) and number of features (1)
