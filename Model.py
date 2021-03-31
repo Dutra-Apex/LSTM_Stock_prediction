7c1fd9a4ccaee5774dccdbb4ddb93378368b4f83
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-03-31 10:35:22 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,7 +1,6 @@
 import math
 import pandas_datareader as web
 import numpy as np
-import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
