2ce53a1ac7005871a2f6a7e68604bc386ae68018
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-04-15 13:16:43 -0400

    Update train_data.py

--- train_data.py
+++ train_data.py
@@ -24,3 +24,6 @@ for i in range(60, len(train_data)):
 
 #Convert both train sets to numpy arrays
 x_train, y_train = np.array(x_train), np.array(y_train)
+
+#Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
