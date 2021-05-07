a557d86a9a1ccb83a169268655d639bb65778504
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-05-07 08:41:21 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -44,3 +44,6 @@ for i in range(60, len(test_data)):
   
 #Convert data to a numpy array
 x_test = np.array(x_test)
+
+# Reshape the data so that it is 3d for LSTM
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
