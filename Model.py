85ed37d11df59554ff841e579e8f49ebeb303de4
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-10-19 18:37:51 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -12,7 +12,7 @@ model = Sequential()
 # Model with 50 neurons, return_sequences = True because we are using another LSTM layer
 # Input shape takes the number of time steps(train.shape[1]) and number of features (1)
 # Number of features is simply the closing price
-model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(LSTM(50, return_sequences=False))
 #model.add(Dense(25))
 model.add(Dense(1))
